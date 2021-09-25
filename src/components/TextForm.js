a8c8f95e21255a0bbbe5d2719399f31461b99940
Bhawana Gaur <bhawanagaur143@gmail.com>
2021-09-25 08:51:06 +0530

    Fix read time issue

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -84,7 +84,7 @@ export default function TextForm(props) {
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h2>Your text Summary</h2>
                 <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
-                <p>{0.008 * text.split(" ").length} Minutes read</p>
+                <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} Minutes read</p>
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Enter something in the textbox to preview it here "}</p>
             </div>
